c9dc142e0564178dea291ad72abbcc5bf4a9a977
Suraj Maharjan <surozb@gmail.com>
2026-07-20 16:57:38 +0545

    Day 1.5: refactor prints to f-strings

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from datetime import date
 
 print('Gamer, Daicha')
 today = date.today()
-print('Today date is:', today)
+print(f'Today date is: {today}')
 target_date = date(2026, 9, 13)
 remaining_days = (target_date - today).days
-print('Remaining days until September 13, 2026:', remaining_days)
+print(f'Remaining days until {target_date:%B %d, %Y}: {remaining_days}')
